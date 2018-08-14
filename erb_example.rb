955e2ce3bc43dd611315e5840c39c920d1d2bc24
Devin Dow <DevinDow@gmail.com>
2018-08-14 09:23:30 -0700

    ERB using array, hash

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,22 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+family = ['Devin', 'Andrea', 'Riley', 'Parker']
+family << 'dog'
+template = ERB.new "family = <%= family %>"
+puts template.result(binding)
+template = ERB.new "
+    <% family.each do |member| %> 
+        <%= member %>
+    <% end %>"
+puts template.result(binding)
+
+ages = {'Devin' => 45, 'Andrea' =>39, 'Riley' => 10, 'Parker' => 7}
+template = ERB.new "
+    <ul>
+        <% ages.each do |person| %>
+            <li><%= person[0] + ' is ' + person[1].to_s %></li>
+        <% end %>
+    </ul>"
+puts template.result(binding)
